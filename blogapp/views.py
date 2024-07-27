c8457b724f5f2f6000d79f0cb98941410ac9c669
josseycodes1 <adewumijosephine1@gmail.com>
2024-07-27 05:20:06 -0700

    template block and extend commit

--- blogapp/views.py
+++ blogapp/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'blogapp/index.html')
 
 def eachpost(request):
-    return render(request, 'eachpost.html')
+    return render(request, 'blogapp/eachpost.html')
 
 def allposts(request):
-    return render(request, 'allposts.html')
+    return render(request, 'blogapp/allposts.html')
